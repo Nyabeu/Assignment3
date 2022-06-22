a9e9b9d1ce927c483fbb2f15ec756b17be9b179d
nyabeu <onyabeu@gmail.com>
2022-06-22 15:22:52 +0200

    assignment3 the basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment3';
+  firstName: string = "fally";
+  password: string = "prk";
+  para = false;
+  timeButton : number[] = [];
+  tmp: number = 0;
+
+  onTogglePara(){
+    this.para = !this.para;
+    this.tmp++;
+    this.timeButton.push(this.tmp);
+    //this.timeButton.push(this.timeButton.Length + 1)
+  }
+
+  getBackground(){
+    let result: string;
+    for (let i=1; i<= this.timeButton.length; i++) {
+      result = (i>=5)? 'blue': 'transparent';
+    }
+    return result;
+  }
 }
